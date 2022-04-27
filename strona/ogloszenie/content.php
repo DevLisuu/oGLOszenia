148ad78df833fa9562f09cea9415423d4c396938
Lisu <everyaccountholder@wp.pl>
2022-04-27 16:47:00 +0200

    wyświetlanie ogłoszeń

--- strona/ogloszenie/content.php
+++ strona/ogloszenie/content.php
@@ -1,3 +1,23 @@
-<p>
+<p><?php
+    @$id = $_GET['id'];
+
+    if(!isset($id)) {
+        header('Location: ../home');
+    }
+
+    $conn = new mysqli('localhost', 'root', '', 'ogloszeniowy');
+
+    $sql = "select * from ogloszenia join susers on ogloszenia.id_autora = susers.id where ogloszenia.id='$id';";
+    $result = $conn->query($sql);
+    $card = $result->fetch_assoc();
     
+    echo(
+    '<h2>"'.$card['tytul'].'"</h2>
+    <p>Autor: '.$card['username'].'<br>Data utworzenia: '.$card['data_dodania']
+    .
+    "</p><hr><p>"
+    .
+    $card['tresc']
+    );
+    ?>
 </p>
