36e6d61c448fbd3fc3b33d9704d18cb477c18170
Lisu <everyaccountholder@wp.pl>
2022-04-27 16:46:49 +0200

    losowanie ogłoszeń

---
+++ strona/random/content.php
@@ -0,0 +1,10 @@
+<p><?php
+    $conn = new mysqli('localhost', 'root', '', 'ogloszeniowy');
+
+    $sql = "select * from ogloszenia order by RAND()";
+    $result = $conn->query($sql);
+    $firstResult = $result->fetch_assoc();
+    
+    header('Location: ../ogloszenie?id='.$firstResult['id']);
+    ?>
+</p>
